e30e71fd63dc979ecfab7280696de6964081d062
BiasDesk1 <shafiquetuhin01>
2022-05-23 20:57:00 +0600

    footer section added

--- src/App.js
+++ src/App.js
@@ -3,22 +3,26 @@ import Navbar from "./components/Navbar";
 import About from "./pages/About";
 import Blog from "./pages/Blog";
 import Contact from "./pages/Contact";
+import Footer from "./pages/Footer";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Purchase from "./pages/Purchase";
 
 function App() {
   return (
-    <Navbar>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/purchase" element={<Purchase />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/blog" element={<Blog />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </Navbar>
+    <>
+      <Navbar>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/purchase" element={<Purchase />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Navbar>
+      <Footer />
+    </>
   );
 }
 
