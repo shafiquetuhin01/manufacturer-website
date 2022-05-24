02f23759c1ea169fb416ea955ab7c4dea4a39b16
BiasDesk1 <shafiquetuhin01>
2022-05-24 16:54:53 +0600

    purchase page updating

--- src/pages/Purchase.js
+++ src/pages/Purchase.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import Details from './Details';
 
 const Purchase = () => {
-    
-    const {perchaseId} = useParams();
+    const {purchaseId} = useParams();
+    const [detailsItem, setDetailsItem] = useState([]);
+        useEffect(()=>{
+        fetch('http://localhost:5000/service')
+        .then(res=>res.json())
+        .then(data=>setDetailsItem(data));
+    },[purchaseId]);
     return (
         <div>
-            <h1 className='text-center text-primary font-bold text-4xl my-5'>You selected the purchase id : {perchaseId}</h1>
+            <h1 className='text-center text-primary font-bold text-4xl my-5'>You selected the purchase id : {purchaseId}</h1>
+            
         </div>
     );
 };
